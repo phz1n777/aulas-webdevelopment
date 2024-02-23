2b28341ecd93772ee2178cceddf0b9a3aececc88
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-23 11:39:17 -0300

    aula

---
+++ Aula01/teste.js
@@ -0,0 +1,6 @@
+const btn1 = document.querySelector('button')
+const listaFilmes = document.querySelector('#listFilmes')
+
+btn1.addEventListener('click',()=>{
+    window.alert("Clicoooooou!!!!")
+})
---
+++ Aula01/teste2.js
@@ -0,0 +1,9 @@
+function imprimir(idade){
+    console.log(idade)
+}
+
+const calcIdade = idade => console.log (idade)
+
+btn1.addEventListener('click',()=>{
+    console.log("Hello")
+})
